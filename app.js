f89fa6884d82c3c4335e0358e8934eaf5c2c1f8b
Vincent Kanimea <vincentk@spc.int>
2020-11-13 16:47:37 +1200

    api posts

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const posts = [{
+   "id": "1581461442206",
+   "title": "This is a New Blog Post",
+   "content": "This is the content! ",
+   "post_image": "uploads/post-image-1581461442199.jpg",
+   "added_date": "1581461442206"
+   }]
+
+
+}]
 
 app.get("/", (req,res)=> {
-   res.status(200).send("Hello World")
+   res.status(200).send(posts);
 });
 
 app.listen(3000, ()=> console.log("Listening on http://localhost:3000"));
